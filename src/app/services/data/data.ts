44f79895f682d51372e20b0cf1f0ab9cd38ec568
Austine <austine.amah@gmail.com>
2022-05-24 15:48:21 +0100

    Image update

--- src/app/services/data/data.ts
+++ src/app/services/data/data.ts
@@ -1,21 +1,29 @@
 export const slides = [
   {
-    image: '../../../assets/images/slider3.jpg',
-    link_to: '',
-  },
-  {
     image: '../../../assets/images/slider1.jpg',
     link_to: '',
   },
   {
-    image: '../../../assets/images/slider2.jpg',
+    image: '../../../assets/images/slider5.jpg',
+    link_to: '',
+  },
+  {
+    image: '../../../assets/images/slider6.jpg',
+    link_to: '',
+  },
+  {
+    image: '../../../assets/images/slider4.jpg',
+    link_to: '',
+  },
+  {
+    image: '../../../assets/images/slider7.jpg',
     link_to: '',
   },
 ];
 
 export const products = [
   {
-    featuredImage: '../../../assets/images/product1.jpeg',
+    featuredImage: '../../../assets/images/product01.png',
     title: 'Inceptos Fusce',
     price: 100.99,
     link: 'product-show',
@@ -24,10 +32,10 @@ export const products = [
     sizes: ['s', 'm', 'l', 'xl'],
     description:
       'Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur rmus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
-    category: 'Packs',
+    category: 'Jackets',
   },
   {
-    featuredImage: '../../../assets/images/product2.jpeg',
+    featuredImage: '../../../assets/images/product02.jpeg',
     title: 'Etiam Magna',
     price: 85.99,
     link: 'product-show',
@@ -39,7 +47,7 @@ export const products = [
     category: 'Jackets',
   },
   {
-    featuredImage: '../../../assets/images/product3.jpeg',
+    featuredImage: '../../../assets/images/product03.jpeg',
     title: 'Sit Vestibulum',
     price: 45.98,
     link: 'product-show',
@@ -52,7 +60,7 @@ export const products = [
   },
 
   {
-    featuredImage: '../../../assets/images/product4.jpeg',
+    featuredImage: '../../../assets/images/product04.jpeg',
     title: 'Parturient Commodo',
     price: 120,
     link: 'product-show',
@@ -64,7 +72,7 @@ export const products = [
     category: 'Jackets',
   },
   {
-    featuredImage: '../../../assets/images/product5.jpeg',
+    featuredImage: '../../../assets/images/product05.jpeg',
     title: 'Magna Justo',
     price: 237.99,
     link: 'product-show',
@@ -73,10 +81,10 @@ export const products = [
     sizes: ['s', 'm', 'l', 'xl'],
     description:
       'Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur rmus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
-    category: 'Shirts',
+    category: 'Packs',
   },
   {
-    featuredImage: '../../../assets/images/product6.jpeg',
+    featuredImage: '../../../assets/images/product06.jpeg',
     title: 'Vestibulum Cursus',
     price: 260.99,
     link: 'product-show',
@@ -85,7 +93,55 @@ export const products = [
     sizes: ['s', 'm', 'l', 'xl'],
     description:
       'Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur rmus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+    category: 'Packs',
+  },
+  {
+    featuredImage: '../../../assets/images/product07.jpeg',
+    title: 'Ligula Tristique',
+    price: 60.99,
+    link: 'product-show',
+    slug: 'ligula-tristique',
+    fav: false,
+    sizes: ['s', 'm', 'l', 'xl'],
+    description:
+      'Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur rmus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+    category: 'Pants',
+  },
+  {
+    featuredImage: '../../../assets/images/product08.jpeg',
+    title: 'Magna Risus',
+    price: 56.99,
+    link: 'product-show',
+    slug: 'magna-risus',
+    fav: false,
+    sizes: ['s', 'm', 'l', 'xl'],
+    description:
+      'Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur rmus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+    category: 'Packs',
+  },
+  {
+    featuredImage: '../../../assets/images/product09.jpeg',
+    title: 'Ipsum Ultricies',
+    price: 35.99,
+    link: 'product-show',
+    slug: 'ipsum-ultricies',
+    fav: false,
+    sizes: ['s', 'm', 'l', 'xl'],
+    description:
+      'Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur rmus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
     category: 'Jackets',
+  },
+  {
+    featuredImage: '../../../assets/images/product10.jpeg',
+    title: 'Ridiculus Lorem',
+    price: 35.99,
+    link: 'product-show',
+    slug: 'ridiculus-lorem',
+    fav: false,
+    sizes: ['l', 'xl'],
+    description:
+      'Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur rmus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+    category: 'Shirts',
   },
 ];
 
